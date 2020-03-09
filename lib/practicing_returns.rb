b3c1c2d0c8d4449113c8c2d3e973f804c7b3d119
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-09 14:08:45 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,6 +1,7 @@
 def hello(array)
   i = 0
   while i < array.length
+  binding.pry
     yield(array[i])
     i += 1
   end
@@ -8,5 +9,5 @@ end
 
 
 
-binding.pry
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
